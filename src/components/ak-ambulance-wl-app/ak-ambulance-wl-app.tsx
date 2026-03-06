63f5df971a806c46f8ec694d076834b596a19738
K-o-m-A <paintester@protonmail.com>
2026-03-06 15:22:29 +0100

    Fix typo

--- src/components/ak-ambulance-wl-app/ak-ambulance-wl-app.tsx
+++ src/components/ak-ambulance-wl-app/ak-ambulance-wl-app.tsx
@@ -56,8 +56,8 @@ export class AkAmbulanceWlApp {
         ? <ak-ambulance-wl-editor entry-id={entryId}
             oneditor-closed={ () => navigate("./list")} >
           </ak-ambulance-wl-editor>
-        : <ak-ambulance-wl-list>
-            onentry-clicked={ (ev: CustomEvent<string>)=> navigate("./entry/" + ev.detail) }
+        : <ak-ambulance-wl-list
+            onentry-clicked={ (ev: CustomEvent<string>)=> navigate("./entry/" + ev.detail) } >
           </ak-ambulance-wl-list>
         }
 
