046094c458347b87a92aea4421905718e16a3168
K-o-m-A <paintester@protonmail.com>
2026-03-16 12:45:00 +0100

    Predpokladaný čas vstupu

--- src/components/ak-ambulance-wl-app/ak-ambulance-wl-app.tsx
+++ src/components/ak-ambulance-wl-app/ak-ambulance-wl-app.tsx
@@ -37,6 +37,7 @@ export class AkAmbulanceWlApp {
   }
 
   render() {
+    console.debug("ak-ambulance-wl-app.render() - path: %s", this.relativePath);
     let element = "list"
     let entryId = "@new"
 
